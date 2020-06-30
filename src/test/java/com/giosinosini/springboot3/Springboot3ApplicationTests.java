0b4995f899464b63840a34c0e34540300f996103
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-06-30 22:33:18 +0100

    Create project

---
+++ src/test/java/com/giosinosini/springboot3/Springboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giosinosini.springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
