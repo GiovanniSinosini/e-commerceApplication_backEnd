606fbb327005b521f01365a5a4ca39efe3c19376
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-07-01 21:48:48 +0100

    Create and testing the domain class called Category

--- src/main/java/com/giosinosini/springboot3/resources/CategoryResource.java
+++ src/main/java/com/giosinosini/springboot3/resources/CategoryResource.java
@@ -1,17 +1,29 @@
 package com.giosinosini.springboot3.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.giosinosini.springboot3.domain.Category;
 
 @RestController
 @RequestMapping(value="categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Category> listar() {
 		
-		return "Rest ON";
+		Category cat1 = new Category (1, "Computing");
+		Category cat2 = new Category (2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 			
 	}	
 }
