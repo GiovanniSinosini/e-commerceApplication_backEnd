c4bba21ec078f18f70b927c95268137e5e17008d
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-07-02 19:21:34 +0100

    Created instantiation operation

--- src/main/java/com/giosinosini/springboot3/Springboot3Application.java
+++ src/main/java/com/giosinosini/springboot3/Springboot3Application.java
@@ -1,13 +1,33 @@
 package com.giosinosini.springboot3;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Springboot3Application {
+import com.giosinosini.springboot3.domain.Category;
+import com.giosinosini.springboot3.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class Springboot3Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;   //dependency
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot3Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
--- src/main/java/com/giosinosini/springboot3/resources/CategoryResource.java
+++ src/main/java/com/giosinosini/springboot3/resources/CategoryResource.java
@@ -1,8 +1,5 @@
 package com.giosinosini.springboot3.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
