d8e81809629423fd5162517863e3927fa220b5fd
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-07-20 21:57:25 +0100

    SpringSecurity => refresh token

---
+++ src/main/java/com/giosinosini/springboot3/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.giosinosini.springboot3.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.giosinosini.springboot3.security.JWTUtil;
+import com.giosinosini.springboot3.security.UserSpringSecurity;
+import com.giosinosini.springboot3.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSpringSecurity user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
