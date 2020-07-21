541c61642bf5f21b3219ef695cb3585b7f830976
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-07-21 20:24:02 +0100

    Amazon S3 => implementation and first upload

--- src/main/java/com/giosinosini/springboot3/Springboot3Application.java
+++ src/main/java/com/giosinosini/springboot3/Springboot3Application.java
@@ -1,11 +1,17 @@
 package com.giosinosini.springboot3;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.giosinosini.springboot3.services.S3Service;
+
 @SpringBootApplication
 public class Springboot3Application implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot3Application.class, args);
@@ -13,6 +19,7 @@ public class Springboot3Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\test.png");
 		
-			}
+	}
 }
