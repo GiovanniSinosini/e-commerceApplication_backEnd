6954a6b76423d5f24d5770b582efa70c16d4cfe3
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-07-27 20:09:04 +0100

    Adding more products to test infinity scroll

--- src/main/java/com/giosinosini/springboot3/services/DBService.java
+++ src/main/java/com/giosinosini/springboot3/services/DBService.java
@@ -79,13 +79,50 @@ public class DBService {
 		Product p10 = new Product(null, "Chair", 45.00);
 		Product p11 = new Product(null, "Wheels", 20.00);
 		
-		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3, p7));
-		cat2.getProducts().addAll(Arrays.asList(p2, p4));
-		cat3.getProducts().addAll(Arrays.asList(p11));
-		cat4.getProducts().addAll(Arrays.asList(p6));
-		cat5.getProducts().addAll(Arrays.asList(p8));
-		cat6.getProducts().addAll(Arrays.asList(p5));
-		cat7.getProducts().addAll(Arrays.asList(p9));
+		Product p12 = new Product(null, "Product 12", 10.00);
+		Product p13 = new Product(null, "Product 13", 10.00);
+		Product p14 = new Product(null, "Product 14", 10.00);
+		Product p15 = new Product(null, "Product 15", 10.00);
+		Product p16 = new Product(null, "Product 16", 10.00);
+		Product p17 = new Product(null, "Product 17", 10.00);
+		Product p18 = new Product(null, "Product 18", 10.00);
+		Product p19 = new Product(null, "Product 19", 10.00);
+		Product p20 = new Product(null, "Product 20", 10.00);
+		Product p21 = new Product(null, "Product 21", 10.00);
+		Product p22 = new Product(null, "Product 22", 10.00);
+		Product p23 = new Product(null, "Product 23", 10.00);
+		Product p24 = new Product(null, "Product 24", 10.00);
+		Product p25 = new Product(null, "Product 25", 10.00);
+		Product p26 = new Product(null, "Product 26", 10.00);
+		Product p27 = new Product(null, "Product 27", 10.00);
+		Product p28 = new Product(null, "Product 28", 10.00);
+		Product p29 = new Product(null, "Product 29", 10.00);
+		Product p30 = new Product(null, "Product 30", 10.00);
+		Product p31 = new Product(null, "Product 31", 10.00);
+		Product p32 = new Product(null, "Product 32", 10.00);
+		Product p33 = new Product(null, "Product 33", 10.00);
+		Product p34 = new Product(null, "Product 34", 10.00);
+		Product p35 = new Product(null, "Product 35", 10.00);
+		Product p36 = new Product(null, "Product 36", 10.00);
+		Product p37 = new Product(null, "Product 37", 10.00);
+		Product p38 = new Product(null, "Product 38", 10.00);
+		Product p39 = new Product(null, "Product 39", 10.00);
+		Product p40 = new Product(null, "Product 40", 10.00);
+		Product p41 = new Product(null, "Product 41", 10.00);
+		Product p42 = new Product(null, "Product 42", 10.00);
+		Product p43 = new Product(null, "Product 43", 10.00);
+		Product p44 = new Product(null, "Product 44", 10.00);
+		Product p45 = new Product(null, "Product 45", 10.00);
+		Product p46 = new Product(null, "Product 46", 10.00);
+		Product p47 = new Product(null, "Product 47", 10.00);
+		Product p48 = new Product(null, "Product 48", 10.00);
+		Product p49 = new Product(null, "Product 49", 10.00);
+		Product p50 = new Product(null, "Product 50", 10.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p12, p13, p14, p15, p16, p17, p18, p19, p20,
+		p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p34, p35, p36, p37, p38,
+		p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50));
+		
 		
 		p1.getCategories().addAll(Arrays.asList(cat1));
 		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
@@ -98,9 +135,60 @@ public class DBService {
 		p9.getCategories().addAll(Arrays.asList(cat7));
 		p10.getCategories().addAll(Arrays.asList(cat2));
 		p11.getCategories().addAll(Arrays.asList(cat3));
-				
+		p12.getCategories().addAll(Arrays.asList(cat1));
+		p13.getCategories().addAll(Arrays.asList(cat1));
+		p14.getCategories().addAll(Arrays.asList(cat1));
+		p15.getCategories().addAll(Arrays.asList(cat1));
+		p16.getCategories().addAll(Arrays.asList(cat1));
+		p17.getCategories().addAll(Arrays.asList(cat1));
+		p18.getCategories().addAll(Arrays.asList(cat1));
+		p19.getCategories().addAll(Arrays.asList(cat1));
+		p20.getCategories().addAll(Arrays.asList(cat1));
+		p21.getCategories().addAll(Arrays.asList(cat1));
+		p22.getCategories().addAll(Arrays.asList(cat1));
+		p23.getCategories().addAll(Arrays.asList(cat1));
+		p24.getCategories().addAll(Arrays.asList(cat1));
+		p25.getCategories().addAll(Arrays.asList(cat1));
+		p26.getCategories().addAll(Arrays.asList(cat1));
+		p27.getCategories().addAll(Arrays.asList(cat1));
+		p28.getCategories().addAll(Arrays.asList(cat1));
+		p29.getCategories().addAll(Arrays.asList(cat1));
+		p30.getCategories().addAll(Arrays.asList(cat1));
+		p31.getCategories().addAll(Arrays.asList(cat1));
+		p32.getCategories().addAll(Arrays.asList(cat1));
+		p33.getCategories().addAll(Arrays.asList(cat1));
+		p34.getCategories().addAll(Arrays.asList(cat1));
+		p35.getCategories().addAll(Arrays.asList(cat1));
+		p36.getCategories().addAll(Arrays.asList(cat1));
+		p37.getCategories().addAll(Arrays.asList(cat1));
+		p38.getCategories().addAll(Arrays.asList(cat1));
+		p39.getCategories().addAll(Arrays.asList(cat1));
+		p40.getCategories().addAll(Arrays.asList(cat1));
+		p41.getCategories().addAll(Arrays.asList(cat1));
+		p42.getCategories().addAll(Arrays.asList(cat1));
+		p43.getCategories().addAll(Arrays.asList(cat1));
+		p44.getCategories().addAll(Arrays.asList(cat1));
+		p45.getCategories().addAll(Arrays.asList(cat1));
+		p46.getCategories().addAll(Arrays.asList(cat1));
+		p47.getCategories().addAll(Arrays.asList(cat1));
+		p48.getCategories().addAll(Arrays.asList(cat1));
+		p49.getCategories().addAll(Arrays.asList(cat1));
+		p50.getCategories().addAll(Arrays.asList(cat1));
+		
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3, p7));
+		cat2.getProducts().addAll(Arrays.asList(p2, p4));
+		cat3.getProducts().addAll(Arrays.asList(p11));
+		cat4.getProducts().addAll(Arrays.asList(p6));
+		cat5.getProducts().addAll(Arrays.asList(p8));
+		cat6.getProducts().addAll(Arrays.asList(p5));
+		cat7.getProducts().addAll(Arrays.asList(p9));
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
+		
+		productRepository.saveAll(Arrays.asList(p12, p13, p14, p15, p16, p17, p18, p19, p20,
+				p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p34, p35, p36, p37, p38,
+				p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50));
 		
 		State state1 = new State(null, "Porto");
 		State state2 = new State(null, "Lisboa");
